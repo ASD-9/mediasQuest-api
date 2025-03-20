ebd34feaebca99336ababc5352eb2b5f6835d2f5
ASD-9 <ryan.chavatte@gmail.com>
2025-03-21 00:28:56 +0100

    fix name of the env variable for db name

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "medias_quest",
+  database: process.env.DB_NAME || "medias_quest",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
