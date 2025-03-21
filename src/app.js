7d1a99f17d8e9aed2c5eb3533d53fdde140ddf7b
ASD-9 <ryan.chavatte@gmail.com>
2025-03-21 23:40:14 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
 app.disable("x-powered-by");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
